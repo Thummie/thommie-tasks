b2ff9c687e48e1648bf2235e5b6473cd4e83be1c
Thommie <53064581+Thummie@users.noreply.github.com>
2024-10-27 17:56:30 +0100

    Added thommie-tasks

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Thummie'
+description 'A small resource that will display the current task.'
+version '1.0.0'
+repository 'https://github.com/Thummie/thommie-tasks/'
+
+ui_page 'html/ui.html'
+
+files {
+	'html/**'
+}
+
+client_script 'client.lua'
+
+lua54 'yes'
